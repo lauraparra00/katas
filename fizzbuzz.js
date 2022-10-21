24a832e15e818a9ab9474409b76dc1599b1c278c
Laura <parra808@hotmail.com>
2022-10-21 12:47:54 +0200

    Readme

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 const fizzbuzz = (num) => {
-    let resultat 
+    
     if (num % 3 == 0){
         resultat = "fizz"
     }
@@ -12,17 +12,8 @@ const fizzbuzz = (num) => {
     if (num % 3 !== 0 && num % 5 !== 0){
         resultat = num 
 }
-return resultat
+return num
 }
 
 
-module.exports = fizzbuzz
-
-function List(){
-    let numList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let numDivisible = num
-    for (let i = 0; i < length ; i ++);
-    
-
-    
-}
+module.exports = fizzbuzz;
